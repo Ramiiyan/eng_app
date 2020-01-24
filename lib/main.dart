d63d92b062be83c2f8d45691875b515ab931ab2c
Ramiiyan <Sriraguhan.2017380@iit.ac.lk>
2020-01-24 20:27:35 +0530

    player have to check OnChanged,OnChangeEnd ln95

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:eng_app/rplayer.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -9,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -28,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +31,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  
+  // final _volumeSubject = BehaviorSubject.seeded(1.0);
+  // final _speedSubject = BehaviorSubject.seeded(1.0);
+  // AudioPlayer _player, _player2, _player3;
+  var url = "https://absolutezerolabs.com/api/v1/lessons/1";
+  
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _player = AudioPlayer();
+  //   _player2 = AudioPlayer();
+  //   _player3 = AudioPlayer();
+    
+  //   _player.setUrl("http://www.freshly-ground.com/misc/music/20060826%20-%20Armstrong.mp3");
+  //   _player2.setUrl("https://www2.iis.fraunhofer.de/AAC/ChID-BLITS-EBU-Narration441-16b.wav");
+  //   // _player3.setUrl("https://absolutezerolabs.com/storage/H0OlYKonkIccUKjatQ0U.mp3");
+  
+  //   response();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  // }
+  
+  // @override
+  // void dispose() {
+  //   _player.dispose();
+  //   _player2.dispose();
+  //   _player3.dispose();
+  //   super.dispose();
+  // }
+  
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          // _audioCard(),
+          RPlayer(
+            audioTitle: "Title 1",
+            audioUrl: "https://www2.iis.fraunhofer.de/AAC/ChID-BLITS-EBU-Narration441-16b.wav",
+          ),
+          // _audioplayer(_player),
+          // _audioCard(_player2),
+          // _audioCard(_player3),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/rplayer.dart
@@ -0,0 +1,192 @@
+
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+class RPlayer extends StatefulWidget {
+  
+  final String audioTitle;
+  final String audioUrl;
+  final Duration duration;
+  final Duration position;
+  final ValueChanged<Duration> onChanged;
+  final ValueChanged<Duration> onChangeEnd;
+
+  RPlayer({
+    @required this.audioTitle,
+    @required this.audioUrl,
+    this.duration,
+    this.position,
+    this.onChanged,
+    this.onChangeEnd,
+  });
+
+  @override
+  _PlayerState createState() => _PlayerState();
+}
+
+class _PlayerState extends State<RPlayer> {
+  AudioPlayer _player;
+  double _dragValue;
+  
+  @override
+  void initState(){
+    super.initState();
+    _player = AudioPlayer();
+    _player.setUrl(widget.audioUrl);
+
+  }
+  @override
+  void dispose(){
+    _player.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return _audioCard();
+    // return SliderTheme(
+    //   data: SliderThemeData(
+    //     thumbShape: RoundSliderThumbShape(enabledThumbRadius: 5),
+    //   ),
+    //   child: Slider(
+    //     min: 0.0,
+    //     max: widget.duration.inMilliseconds.toDouble(),
+    //     value: _dragValue ?? widget.position.inMilliseconds.toDouble(),
+    //     onChanged: (value) {
+    //       setState(() { _dragValue = value; });
+    //       if (widget.onChanged != null) {
+    //         widget.onChanged(Duration(milliseconds: value.round()));
+    //       }
+    //     },
+    //     onChangeEnd: (value) {
+    //       _dragValue = null;
+    //       if (widget.onChangeEnd != null) {
+    //         widget.onChangeEnd(Duration(milliseconds: value.round()));
+    //       }
+    //     },
+    //   ),
+    // ); 
+  }
+  Widget _audioCard(){
+    return  StreamBuilder<AudioPlaybackState>(
+      stream: _player.playbackStateStream,
+      builder: (context, snapshot) {
+        final state = snapshot.data;
+        return Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child:ListTile( 
+               leading: _playOrPause(state,_player),
+              title: Text(widget.audioTitle,
+                style: TextStyle(fontSize: 22.0),
+              ),
+              subtitle: StreamBuilder<Duration>(
+                stream: _player.durationStream,
+                builder: (context, snapshot) {
+                  final duration = snapshot.data ?? Duration.zero;
+                  return StreamBuilder<Duration>(
+                    stream: _player.getPositionStream(),
+                    builder: (context, snapshot) {
+                      var position = snapshot.data ?? Duration.zero;
+                      if (position > duration) {
+                        position = duration;
+                      }
+                      return _seekbar(duration, position);
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+  Widget _playOrPause( AudioPlaybackState state, AudioPlayer _player){
+    if (state == AudioPlaybackState.playing)
+      return IconButton(
+        icon: Icon(Icons.pause_circle_outline),
+        iconSize: 40.0,
+        color: Colors.blue,
+        onPressed: _player.pause,
+      );
+    else if (state == AudioPlaybackState.buffering || state == AudioPlaybackState.connecting)
+      return Container(
+        margin: EdgeInsets.all(4.0),
+        width: 38.0,
+        height: 38.0,
+        child: CircularProgressIndicator(),
+      );
+    else                      
+      return IconButton(
+        icon: Icon(Icons.play_circle_outline),
+        iconSize: 40.0,
+        onPressed: _player.play,
+      );
+      // IconButton(
+      //   icon: Icon(Icons.stop),
+      //   iconSize: 64.0,
+      //   onPressed: state == AudioPlaybackState.stopped ||
+      //           state == AudioPlaybackState.none
+      //       ? null
+      //       : _player.stop,
+      // ),
+  }
+
+  Widget _seekbar(Duration duration, Duration position){
+    return SliderTheme(
+      data: SliderThemeData(
+        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 4),
+      ),
+      child: Slider(
+        min: 0.0,
+        max: duration.inMilliseconds.toDouble(),
+        value: _dragValue ?? position.inMilliseconds.toDouble(),
+        onChanged: (value) {
+          setState(() {
+             _dragValue = value;
+          });
+          if (widget.onChanged != null) {
+            widget.onChanged(Duration(milliseconds: value.round()));
+          }
+        },
+        onChangeEnd: (value) {
+          _dragValue = null;
+          if (widget.onChangeEnd != null) {
+            widget.onChangeEnd(Duration(milliseconds: value.round()));
+          }
+        },
+      ),
+    );
+  }
+  //volume, speed adjust
+        // Text("Volume"),
+        // StreamBuilder<double>(
+        //   stream: _volumeSubject.stream,
+        //   builder: (context, snapshot) => Slider(
+        //     divisions: 20,
+        //     min: 0.0,
+        //     max: 2.0,
+        //     value: snapshot.data ?? 1.0,
+        //     onChanged: (value) {
+        //       _volumeSubject.add(value);
+        //       _player.setVolume(value);
+        //     },
+        //   ),
+        // ),
+        // Text("Speed"),
+        // StreamBuilder<double>(
+        //   stream: _speedSubject.stream,
+        //   builder: (context, snapshot) => Slider(
+        //     divisions: 10,
+        //     min: 0.5,
+        //     max: 1.5,
+        //     value: snapshot.data ?? 1.0,
+        //     onChanged: (value) {
+        //       _speedSubject.add(value);
+        //       _player.setSpeed(value);
+        //     },
+        //   ),
+        // ),
+}
